9b6242c02ffb29dd9953427286682f2a2616600e
Nunna Pooja <141369961+NunnaPooja@users.noreply.github.com>
2025-06-30 19:05:48 +0530

    Time: 0 ms (100%), Space: 17.6 MB (36.34%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    bool dfs(int node,int color,vector<int>& colors,vector<vector<int>>& graph){
-        colors[node]=color;
-        for(auto it:graph[node]){
-            if(colors[it]==-1){
-                if(dfs(it,!color,colors,graph)==false) return false; //return statement was not written before hence 
-                //gave wrong answer
-            } 
-            else if(colors[it]==color) return false;
-        }
-        return true;
-    }
     bool isBipartite(vector<vector<int>>& graph) {
         //if we are able to color with alterante colors then we can call that fraph as bipartite
+        //BFS soln
         int n=graph.size();
         vector<int>colors(n,-1); //assigning color as -1 representing non colored node
-        for(int i=0;i<n;i++){
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){ //need to check for disconnected components as well
+        /*For example, consider a graph with two disconnected bipartite subgraphs:
+        Component 1: 
+        1↔2
+        Component 2: 
+        3↔4
+        Both components can still belong to the same two teams: {1, 3} and {2, 4}.
+        */
             if(colors[i]==-1){ //call dfs for non colored node
-                if(dfs(i,0,colors,graph)==false) return false;
+                q.push({i,0});
+                colors[i]=0;
+                while(!q.empty()){
+                    auto [node,clr]=q.front();
+                    q.pop();
+                    for(auto it:graph[node]){
+                        if(colors[it]==-1){
+                            q.push({it,!clr});
+                        colors[it]=!clr;
+                        }
+                        else{
+                            if(colors[it]==clr) return false;
+                        }
+                    }
+                }
             } 
         }
         return true;
