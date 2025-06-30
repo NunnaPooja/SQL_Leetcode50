8325a01cf40dbbe2df999fc6f39cb5d42fedca7c
Nunna Pooja <141369961+NunnaPooja@users.noreply.github.com>
2025-06-30 19:00:53 +0530

    Time: 0 ms (100%), Space: 17.2 MB (87.07%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    bool dfs(int node,int color,vector<int>& colors,vector<int>&visited,vector<vector<int>>& graph){
-        visited[node]=1;
+    bool dfs(int node,int color,vector<int>& colors,vector<vector<int>>& graph){
         colors[node]=color;
         for(auto it:graph[node]){
-            if(visited[it]!=1){
-                if(dfs(it,!color,colors,visited,graph)==false) return false; //return statement was not written before hence 
+            if(colors[it]==-1){
+                if(dfs(it,!color,colors,graph)==false) return false; //return statement was not written before hence 
                 //gave wrong answer
             } 
             else if(colors[it]==color) return false;
@@ -15,11 +14,10 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         //if we are able to color with alterante colors then we can call that fraph as bipartite
         int n=graph.size();
-        vector<int>colors(n,0);
-        vector<int>visited(n,0);
+        vector<int>colors(n,-1); //assigning color as -1 representing non colored node
         for(int i=0;i<n;i++){
-            if(visited[i]!=1){
-                if(dfs(i,0,colors,visited,graph)==false) return false;
+            if(colors[i]==-1){ //call dfs for non colored node
+                if(dfs(i,0,colors,graph)==false) return false;
             } 
         }
         return true;
