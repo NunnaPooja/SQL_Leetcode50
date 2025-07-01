c2ce65b85aec218b4fb7a7cfbd6c960ba70c7139
Nunna Pooja <141369961+NunnaPooja@users.noreply.github.com>
2025-07-01 19:41:50 +0530

    Time: 3 ms (66.69%), Space: 8.2 MB (16.76%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -3,8 +3,14 @@ public:
     int fib(int n) {
        //basecase
        if(n==0) return 0;
-       if(n==1||n==2) return 1;
+       if(n==1) return 1;
+       vector<int>dp(n+1); //here if n==0 it creates dp of size 1 then dp[1] will be initialised causing run time error
+       dp[0]=0;
+       dp[1]=1;
        //recursion
-       return fib(n-1)+fib(n-2);
+       for(int i=2;i<=n;i++){ //u can run loop from 2
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
 };
