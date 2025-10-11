fbe03bdca7b112f545c0f34279af533cc6df644a
Nunna Pooja <141369961+NunnaPooja@users.noreply.github.com>
2025-10-11 11:47:22 +0530

    Time: 83 ms (44.22%), Space: 22.6 MB (26.17%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,54 +1,28 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        //bfs
-        //add wordList elements into set for an easy search
-    /*Search Complexity:
-    std::vector:
-    If unsorted, linear search O(N)
-    If sorted, binary search can be used O(logn)).
-
-    std::set: Search is always logarithmic (O(logn)) because of its tree structure.
-    Ease of Use:
-    Searching in a vector requires you to manage sorting explicitly if you want efficient search (e.g., std::sort followed by std::binary_search).
-    In a set, elements are always sorted, and searching is straightforward using methods like set.find().*/
-    unordered_set<string>st(wordList.begin(),wordList.end());
         queue<pair<string,int>>q;
+        unordered_set<string>set1(wordList.begin(),wordList.end());
+        if(set1.count(endWord)<=0) return 0;
         q.push({beginWord,1});
-        st.erase(beginWord); //erase from set as well.
-        //example startword='a' endword='c',wordlist=['a' ,'b' ,'c'] 
-        //if u dont remove 'a' initally then while checking for char loop it will again store 'a' steps as steps+1 
-        //even if u dont remove intial check it gives answer. due to check=> if(word==endWord) return level.
+        unordered_set<string>vis;
+        vis.insert(beginWord);
         while(!q.empty()){
-            string word=q.front().first;
-            int level=q.front().second;
+            auto [word,steps]=q.front();
             q.pop();
-            if(word==endWord) return level;
-            // for(auto it:word){
-            //    char original=it;
-                // for(char c='a';c<='z';c++){
-                //     it=c;
-                //   if(st.find(word)!=st.end()){
-                //     q.push({word,level+1});  
-                //   st.erase(word);
-                //   }
-                // }
-                // it=original;
-                /*Your implementation has a logical issue: 'it' is a copy of the character in word and not a reference.
-                 This means modifying it will not update the original word. To fix this, you need to iterate over word
-                  by reference or directly access its characters using an index.*/
-                  for(int i=0;i<word.size();i++){
-                    char original=word[i];
-                    for(char c='a';c<='z';c++){
-                        word[i]=c;
-                        if(st.find(word)!=st.end()){
-                            q.push({word,level+1});
-                            st.erase(word);
-                        }
+            if(word==endWord) return steps;
+            for(int i=0;i<word.size();i++){
+                char org=word[i];
+                for(char c='a';c<='z';c++){
+                    word[i]=c;
+                    if(set1.count(word)>0 && vis.count(word)<=0){
+                        q.push({word,steps+1});
+                        vis.insert(word);
                     }
-                    word[i]=original;
-                  }
+                }
+                word[i]=org;
+            }
         }
-        return 0;
+        return 0; //return -1 if no such sequence found
     }
 };
