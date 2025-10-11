fde36fe1af2006a9d5b9bcfbdae9d8369411a3dd
Nunna Pooja <141369961+NunnaPooja@users.noreply.github.com>
2025-10-11 12:42:23 +0530

    Time: 4 ms (56.16%), Space: 19 MB (17.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-vector<int>toposort(int n,vector<vector<int>>&adjlist){
-    vector<int>ans;
-        queue<int>q;
-        vector<int>indegrees(n,0);
-        for(int i=0;i<n;i++){
-           for(auto it:adjlist[i]){
-               indegrees[it]++;
-           } 
-        }
-        //add node with indegrees=0 into the queue
-       for(int i=0;i<n;i++){
-           if(indegrees[i]==0) q.push(i);
-       } 
-       while(!q.empty()){
-           int node=q.front();
-           q.pop();
-           ans.push_back(node);
-           for(auto it: adjlist[node]){
-               //remove the edge between node and adj nodes
-               indegrees[it]--;
-               if(indegrees[it]==0) q.push(it); //if adj nodes has no indegrees it means it can be added to queue for 
-               //being in topo sort order
-           }
-       }
-       return ans;
-}
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-         //numCouses is the no.of vertices or say no.of nodes
-        vector<vector<int>> adjlist(numCourses); 
-        vector<int>ans;
+        vector<int>indegrees(numCourses,0);
+        vector<vector<int>>adj(numCourses);
         for(auto it:prerequisites){
-           adjlist[it[1]].push_back(it[0]);
-           /*Using it.first and it.second won't work because 'it' is a vector<int>, not a pair<int, int>.*/
+            int u=it[0];
+            int v=it[1];
+            indegrees[u]++;
+            adj[v].push_back(u);
         }
-        vector<int>topo= toposort(numCourses,adjlist);
-        if(topo.size()<numCourses) return ans; //if the toposort size is anywhere less than no.of nodes
-        //it means that would be coz of cycle and problem statement given that u need to return empty string
-        return topo;
+        queue<int>q;
+        vector<int>topo;
+        for(int i=0;i<numCourses;i++){ //indexing till numcourses-1
+            if(indegrees[i]==0) q.push(i);
+        }
+        int cnt=0; //toposort count
+        while(!q.empty()){
+            auto node=q.front();
+            topo.push_back(node);
+            cnt++;
+            if(cnt==numCourses) return topo;
+            q.pop();
+            for(auto neig:adj[node]){
+                indegrees[neig]--;
+                if(indegrees[neig]==0){
+                    q.push(neig);
+                }
+            }
+        }
+        return {};
     }
 };
